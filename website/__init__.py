1271b3ffdbd62a05b3c7e387914fec4791d5c697
YaronL16 <lavyaron@gmail.com>
2023-07-15 14:34:08 +0300

    added views and auth routes

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,9 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'ogni tua piccola lacrima e oceano sopra il mio viso'
 
+    from .views import views
+    from .auth import auth
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
