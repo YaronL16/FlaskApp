4d2a426b8f65a3f17dded7f7cd76a93252fc8cd6
trix <trixteam.sys@gmail.com>
2024-02-24 03:59:31 +0200

    added database

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+# Define Database
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 # Create Flask App
 def create_app():
     app = Flask(__name__)
+
+    # Define encryption key
     app.config['SECRET_KEY'] = 'ogni tua piccola lacrima e oceano sopra il mio viso'
+
+    # Define database file location
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    # Define Flask app for the database
+    db.init_app(app)
 
     # Import and register blueprints
     from .views import views
@@ -11,4 +25,15 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    # Set up database
+    from .models import User, Note
+    create_database(app)
+
     return app
+
+def create_database(app):
+    with app.app_context():
+        if not path.exists('instance/' + DB_NAME):
+            db.create_all()
+            print("Created Database")
+    
---
+++ website/models.py
@@ -0,0 +1,18 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+# Define notes schema
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    owner = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+# Define user schema
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    username = db.Column(db.String(150))
+    notes = db.relationship('Note')
